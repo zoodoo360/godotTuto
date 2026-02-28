153c9c4aa6f55913d253bbdf803f033886dc57f1
Mike <mikebardet@gmail.com>
2026-02-27 19:00:49 -0500

    laser collision and vanishing

--- scenes/projectiles/laser.gd
+++ scenes/projectiles/laser.gd
@@ -4,6 +4,9 @@ const laser_scene : PackedScene = preload("res://scenes/projectiles/laser.tscn")
 var direction: Vector2 = Vector2.RIGHT
 @export var speed:float = 1.0
 
+func _ready():
+	$TTL.start()
+
 func _physics_process(delta):
 	movelaser(delta)
 
@@ -17,3 +20,13 @@ static func createInstance(spawnPosition:Vector2, spawnDirection:Vector2, spawnT
 	
 func movelaser(delta):
 	position += speed * direction * delta
+
+func _on_body_entered(body):
+	if "gotHit" in body:
+		body.gotHit()
+	queue_free()
+
+
+func _on_ttl_timeout():
+	print("I'm fading")
+	queue_free()
