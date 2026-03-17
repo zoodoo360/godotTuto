d29aa5510314566a4f001c669ff99ac9522e339c
Mikeb <mikebardet@gmail.com>
2026-03-17 18:36:43 -0400

    fix scene transition

--- globals/transition_layer.gd
+++ globals/transition_layer.gd
@@ -4,3 +4,6 @@ func changeScene(target:String) -> void:
 	$AnimationPlayer.play("FadeToBlack")
 	await $AnimationPlayer.animation_finished
 	get_tree().call_deferred("change_scene_to_file", target)
+	$AnimationPlayer.play_backwards("FadeToBlack")
+	await $AnimationPlayer.animation_finished
+	
